3f98a56bc5f403b45bb9dbce023c274d04ad9a59
matiivan <matiasivan.moussompes@gmail.com>
2023-07-25 16:16:49 -0300

    agrego json modifico storage

--- javascrip/app.js
+++ javascrip/app.js
@@ -1,65 +1,4 @@
 
-
-  // class BazarLine {
-  //   constructor() {
-  //     this.carrito = [];
-  
-  
-  //     this.loadProductos();
-  
-  //     productos.forEach(producto => {
-  //       const cardElement = document.getElementById(producto.nombre);
-  //       cardElement.addEventListener("click", () => {
-  //         this.getProductPrice(producto);
-  //       });
-  //     });
-  //   }
-
-  //   loadProductos() {
-      
-  //     const url = './productos.json';
-  //     fetch(url)
-  //       .then(response => response.json())
-  //       .then(data => {
-  //         this.productos = data;
-  //       })
-  //       .catch(error => {
-  //         console.error('Error al cargar los productos:', error);
-  //       });
-  //   }
-  
-  //   getProductPrice(producto) {
-  //     const { nombre, precioSinIVA } = producto;
-  
-  //     Swal.fire({
-  //       title: '¿Desea comprar este producto?',
-  //       text: `No podrá revertir esta acción para el producto ${nombre}!`,
-  //       icon: 'question',
-  //       showCancelButton: true,
-  //       confirmButtonText: 'Sí, comprar',
-  //       cancelButtonText: 'No, cancelar',
-  //       reverseButtons: true,
-  //       customClass: {
-  //         confirmButton: 'btn btn-success',
-  //         cancelButton: 'btn btn-danger'
-  //       },
-  //       buttonsStyling: false
-  //     }).then((result) => {
-  //       if (result.isConfirmed) {
-  //         const precioConIVA = precioSinIVA * 1.21;
-  //         this.carrito.push({ producto: nombre, precio: precioConIVA });
-  //         this.showAlert(`Producto ${nombre} agregado al carrito de compras.`);
-  //         console.log(`Producto ${nombre} agregado al carrito.`);
-  //       } else if (result.dismiss === Swal.DismissReason.cancel) {
-  //         this.showAlert("La acción ha sido cancelada.");
-  //       }
-  //     });
-  //   }
-  
-  
-  // const bazarLine = new BazarLine();
-  
-
   class BazarLine {
     constructor() {
       this.carrito = [];
@@ -68,6 +7,7 @@
       this.productos = [];
   
       this.loadProductos();
+      
   
       const buttons = document.querySelectorAll('.btn.btn-primary');
       buttons.forEach(button => {
@@ -80,7 +20,7 @@
     }
   
     loadProductos() {
-      const url = './productos.json';
+      const url = '../productos.json';
       fetch(url)
         .then(response => response.json())
         .then(data => {
@@ -134,8 +74,22 @@
       });
     }
   }
-  
-  
+  document.getElementById('enviar').addEventListener('click', function () {
+    
+    var hayError = true;
+
+    if (hayError) {
+
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'Hubo un problema al enviar el formulario. Por favor, inténtalo de nuevo.',
+      });
+    } 
+  });
+
   const bazarLine = new BazarLine();
-  
+
+
+
   
